2efcf93fd39623284ee688056b0833777bd6233f
吴海超 <712641411@qq.com>
2015-07-30 11:46:53 +0800

    Initial Commit

---
+++ WHC_PhotoCameraChoicePictureDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WHC_PhotoCameraChoicePictureDemo
+//
+//  Created by 吴海超 on 15/7/30.
+//  Copyright (c) 2015年 吴海超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
